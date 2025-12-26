84ffcbde6e22ee078ac036dac324d9e6c9ddf1b4
Jonathan Miner <cp_n18@yahoo.com>
2025-12-26 07:07:18 -0500

    Added a few more API endpoints

--- gqrx/gqrx.py
+++ gqrx/gqrx.py
@@ -1,5 +1,9 @@
 """
 GQRX API
+
+Partial implementation based on:
+
+https://github.com/gqrx-sdr/gqrx/blob/master/resources/remote-control.txt
 """
 import socket
 import time
@@ -15,6 +19,7 @@ class GQRX():
 
     def __del__(self):
         """ Close the connection """
+        self.close()
         self.client.close()
 
     def get_response(self):
@@ -42,11 +47,81 @@ class GQRX():
         self.client.send(cmd.encode())
         return self.status()
 
+    def get_gain(self):
+        """ Get Audio Gain """
+        self.client.send(b"l AF\n")
+        data = self.get_response()
+        return int(data)
+
+    def set_gain(self,gain):
+        """ Set Audio Gain """
+        cmd = "L AF %f\n" % gain
+        self.client.send(cmd.encode())
+        return self.status()
+
+    def get_demod_mode(self):
+        """ Get Demodulator Mode """
+        cmd = "m\n"
+        self.client.send(cmd.encode())
+        data = self.get_response()
+        return str(data)
+
+    def set_demod_mode(self,mode):
+        """ Set Demodulator Mode """
+        cmd = "M %s\n" % mode
+        self.client.send(cmd.encode())
+        return self.status()
+
     def get_signal_strength(self):
         """ Get Signal Strength """
         self.client.send(b"l STRENGTH\n")
         data = self.get_response()
         return float(data)
+
+    def get_squelch(self):
+        """ Get Squelch Threshold """
+        cmd = "l SQL\n"
+        self.client.send(cmd.encode())
+        data = self.get_response()
+        return float(data)
+
+    def set_squelch(self,dbfs):
+        """ Set Squelch Threshold """
+        cmd = "L SQL %s\n" % dbfs
+        self.client.send(cmd.encode())
+        return self.status()
+
+    def get_record(self):
+        """ Get Recorder Status """
+        cmd = "u RECORD\n"
+        self.client.send(cmd.encode())
+        data = self.get_response()
+        return str(data)
+
+    def set_record(self,status):
+        """ Set Recorder Status """
+        cmd = "U RECORD %s\n" % status
+        self.client.send(cmd.encode())
+        return self.status()
+
+    def close(self):
+        """ Close Connection """
+        cmd = "c\n"
+        self.client.send(cmd.encode())
+        return self.status()
+
+    def aos(self):
+        """ Acquisition of Signal """
+        cmd = "AOS\n"
+        self.client.send(cmd.encode())
+        return self.status()
+
+    def los(self):
+        """ Loss of Signal """
+        cmd = "LOS\n"
+        self.client.send(cmd.encode())
+        return self.status()
+
 
 def scan(low_hz, high_hz, step_hz=1e3, threshold_db=-40, pause_sec=1):
     """ Demo Code for Scanning """
